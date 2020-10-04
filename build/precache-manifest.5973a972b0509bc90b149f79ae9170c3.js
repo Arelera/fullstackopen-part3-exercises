f4f6a51f64d441306b8a8979bf4bae63c3004ae6
Arel <arelcode@gmail.com>
2020-10-04 22:11:17 +0300

    Build added

---
+++ build/precache-manifest.5973a972b0509bc90b149f79ae9170c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c367fc0e93dd907a9ceeb7feeb8e81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9361855e9a87b0084c28",
+    "url": "/static/css/main.228cf2ff.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9361855e9a87b0084c28",
+    "url": "/static/js/main.7a91148b.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
---
+++ build/precache-manifest.c63c4ab9363a047f16b1c3d71d74d388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6332f96138ae099fcc6e97d928139ce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54be0a02cbb35af0534",
+    "url": "/static/css/main.ad7cb0f2.chunk.css"
+  },
+  {
+    "revision": "5025582f0bba5a0d1c75",
+    "url": "/static/js/2.d0ffda05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0ffda05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54be0a02cbb35af0534",
+    "url": "/static/js/main.79b4d37f.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
